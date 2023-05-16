f19573ffd67fdbed8d53cf9d49141b232c1910f7
Abdul Basit <77069442+abdulbasit-ab@users.noreply.github.com>
2023-05-16 12:03:49 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 setup(
-   name='processSAR',
+   name='snapcli',
    version='1.0',
    description='SNAP Command Line Tutorial',
    author='Abdul Basit',
    author_email='abdulbasit@itu.edu.pk',
-   packages=['processSAR']
+   packages=['snapcli']
 )
