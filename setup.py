245b6c339bb2f21d2c1fdfc081227a4a73f703d7
Abdul Basit <77069442+abdulbasit-ab@users.noreply.github.com>
2023-05-16 11:27:53 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name = 'snapcli',
     version = '0.1.0',
-    url = '',
+    url = 'https://github.com/abdulbasit-ab/snapcli',
     description = '',
-    packages = find_packages(),
-    install_requires = [
-        # Github Private Repository
-        'snapcli @ https://github.com/abdulbasit-ab/snapcli.git'
-    ]
+#     packages = find_packages(),
+#     install_requires = [
+#         # Github Private Repository
+#         'snapcli @ https://github.com/abdulbasit-ab/snapcli.git'
+#     ]
 )
