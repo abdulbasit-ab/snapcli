b255d3e426939904c793a5c3757596d90c0d724d
Abdul Basit <77069442+abdulbasit-ab@users.noreply.github.com>
2023-05-16 12:02:01 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setup(
    description='SNAP Command Line Tutorial',
    author='Abdul Basit',
    author_email='abdulbasit@itu.edu.pk',
-   packages=['processSAR'],
-   install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+   packages=['processSAR']
 )
