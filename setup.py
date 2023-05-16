f906cfff0f38d34adaa4d15d82118ceee10a9708
Abdul Basit <77069442+abdulbasit-ab@users.noreply.github.com>
2023-05-16 11:50:31 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = '0.1.0',
     url = 'https://github.com/abdulbasit-ab/snapcli',
     description = '',
-    packages = find_packages(),
+    packages = ['processSAR'],
 #     install_requires = [
 #         # Github Private Repository
 #         'snapcli @ https://github.com/abdulbasit-ab/snapcli.git'
